167119f97e556348e0fce71a72cafc631c7623d8
HiHighs <degroote.jonas@hotmail.com>
2024-12-09 19:43:04 +0100

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
@@ -8,16 +8,16 @@ import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
-    <BrowserRouter basename='/gromidin'>
+    <Router>
       <ScrollToTop />
       <Routes>
-        <Route index element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/mission' element={<Mission />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
